cb829fd1206f5e5e8dd825048759f62ab7f2fa0f
dsumners <jandcoke@gmail.com>
2021-02-18 11:52:04 -0600

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,7 +67,10 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.error('Invalid login attempt')
             return redirect(url_for('login'))
+        else:
+            app.logger.error('{} logged in successfully'.format( user.username))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -85,8 +88,11 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = None
+        # Acquire a token from a built msal app, along with the appropriate redirect URI
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+            request.args['code'],
+            scopes=Config.SCOPE,
+            redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
@@ -110,43 +116,24 @@ def logout():
 
     return redirect(url_for('login'))
 
-
-
 def _load_cache():
-
     cache = msal.SerializableTokenCache()
-
     if session.get('token_cache'):
-
         cache.deserialize(session['token_cache'])
-
     return cache
 
-
 def _save_cache(cache):
-
     if cache.has_state_changed:
-
         session['token_cache'] = cache.serialize()
 
-
-
 def _build_msal_app(cache=None, authority=None):
-
     return msal.ConfidentialClientApplication(
-
         Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
-
         client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
-
-
 def _build_auth_url(authority=None, scopes=None, state=None):
-
     return _build_msal_app(authority=authority).get_authorization_request_url(
-
         scopes or [],
-
         state=state or str(uuid.uuid4()),
-
         redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+
