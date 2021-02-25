3028d989a18f87dd3cda58417a20c6011b3596b7
dsumners <jandcoke@gmail.com>
2021-02-25 12:21:07 -0600

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ This script runs the FlaskWebProject application using a development server.
 
 from os import environ
 from FlaskWebProject import app
+import logging
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
