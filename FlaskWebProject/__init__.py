98209086d492f3bb2104ec17c809f4848bd2dc97
dsumners <jandcoke@gmail.com>
2021-02-25 14:30:25 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,20 +1,15 @@
-import logging
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
-
 app = Flask(__name__)
 app.config.from_object(Config)
-gunicorn_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
-app.logger.info('Server Started')
+# TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.WARNING)
+#SOURCE: Solution: Monitoring and Logging in Azure
+app.logger.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+app.logger.addHandler(streamHandler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
 import FlaskWebProject.views
-
