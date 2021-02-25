b13a388839f23de36099b5d9e68e15891b57d96c
dsumners <jandcoke@gmail.com>
2021-02-25 12:44:45 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 import sys
-import .header import header
+from .header import header
 
 app = Flask(__name__)
 app.config.from_object(Config)
