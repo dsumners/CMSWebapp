68cf935dcf851b38146280548a7d170b1beb6254
dsumners <jandcoke@gmail.com>
2021-02-25 12:48:26 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -8,7 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 import sys
-from .header import header
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,7 +17,6 @@ streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
 app.logger.info('App startup')
-app.logger.info(header)
 
 Session(app)
 db = SQLAlchemy(app)
