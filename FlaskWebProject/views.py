190d43bbebe7642cba44636d3bd8f27aab55ccef
dsumners <jandcoke@gmail.com>
2021-03-09 14:47:51 -0600

    Update views.py
    Modified logging per screenshot from Dhruv K Mentor Udacity...

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,13 +67,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.warning('Invalid username or password')
+            app.logger.info('%s failed to log in, Invalid username or password', form.username.data)
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info('%s Successfully Logged In', user.username)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            app.logger.warning('Successful User Login!!!')
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
