98ab1f350b6e582465030637c6d72dafddbf6446
dsumners <jandcoke@gmail.com>
2021-03-10 09:19:04 -0600

    Update views.py
    switched line 73 and line 74

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -70,8 +70,8 @@ def login():
             app.logger.info('%s failed to log in, Invalid username or password', form.username.data)
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
         app.logger.info('%s Successfully Logged In', user.username)
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
