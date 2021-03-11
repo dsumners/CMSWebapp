e1af4364ded9e7d3045065ef23640e0c6230e13a
dsumners <jandcoke@gmail.com>
2021-03-10 18:54:55 -0600

    Update views.py
    Add flash on line 77

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,10 +71,11 @@ def login():
             flash('Invalid username or password')
         return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info('Successfully Logged In')   
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-           next_page = url_for('home')
+            app.logger.info('Successfully Logged In')
+            flash('Successful Login')
+            next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
