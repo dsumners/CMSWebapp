841af74dab46eec1bcacc75cecf485e4e05163b2
dsumners <jandcoke@gmail.com>
2021-03-11 11:12:42 -0600

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,13 +69,13 @@ def login():
         if user is None or not user.check_password(form.password.data):
             app.logger.info('%s failed to log in, Invalid username or password', form.username.data)
             flash('Invalid username or password')
-        return redirect(url_for('login'))
+            return redirect(url_for('login'))
         app.logger.info('%s Successfully Logged In', user.username)
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-        return redirect(next_page)
+            return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
